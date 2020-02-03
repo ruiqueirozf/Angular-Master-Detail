1d6a67421d50e56aebdf6a55aa89a8f30e960102
rui <ruiqueiroz@frwk.com.br>
2020-02-03 17:06:48 -0300

    adding category model

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,18 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
+
+export class InMemoryDatabase implements InMemoryDbService {
+
+    createDb() {
+        const categories: Category [] = [
+        { id: 1, name: 'Moradia', description: 'Contas da Casa'},
+        { id: 2, name: 'Saúde', description: 'Plano de Saúde'},
+        { id: 3, name: 'Lazer', description: 'Cinema, praia, viagem'},
+        { id: 4, name: 'Salário', description: 'Recebimento de Salário'},
+        { id: 5, name: 'Freelas', description: 'Projetos de freelancer'},
+        { id: 6, name: 'Alimentação', description: 'Café da manha, almoço'}
+    ];
+
+        return { categories };
+    }
+}
